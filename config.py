e459eabf61e7cd5e318aa4c57f37c603fd842023
harsh1243 <78589481+harsh1243@users.noreply.github.com>
2025-11-30 12:15:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+#Database & Paths
+DB_PATH = "twitter_clone.db"
+UPLOAD_DIR = "uploads"
+PROFILE_PIC_DIR = os.path.join(UPLOAD_DIR, "profiles")
+POST_IMAGE_DIR = os.path.join(UPLOAD_DIR, "posts")
+
+#Blockchain Config
+SUI_RPC_URL = "https://fullnode.mainnet.sui.io:443"
+
+os.makedirs(PROFILE_PIC_DIR, exist_ok=True)
+os.makedirs(POST_IMAGE_DIR, exist_ok=True)
